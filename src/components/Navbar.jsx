98a265fcfe034e6033b77ab845d7c7981a8a5b73
LeandroOnline <105742869+LeandroOnline@users.noreply.github.com>
2023-02-24 17:01:13 -0300

    feat: fix responsive menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,47 +8,53 @@ const xicon = "https://cdn-icons-png.flaticon.com/512/7693/7693271.png";
 function Navbar() {
   const [open, setOpen] = useState(false);
   return (
-    <nav className="navbar">
-      <NavLink>
-        <img
-          className="logo"
-          src="https://cdn-icons-png.flaticon.com/512/2798/2798097.png"
-          alt=""
-        />
-      </NavLink>
+    <div className="navbarfull">
+      <nav className="navbar">
+        <NavLink>
+          <img
+            className="logo"
+            src="https://cdn-icons-png.flaticon.com/512/2798/2798097.png"
+            alt=""
+          />
+        </NavLink>
 
-      <div className="menu-icon" onClick={() => setOpen(!open)}>
-        <img src={open ? xicon : menuicon} className="icon" />
-      </div>
-
-      <div className={open ? "nav-menu active" : "nav-menu"}>
-        <div className="nav-item">
-          <NavLink to="/" className="nav-links" onClick={() => setOpen(false)}>
-            Home
-          </NavLink>
+        <div className="menu-icon" onClick={() => setOpen(!open)}>
+          <img src={open ? xicon : menuicon} className="icon" />
         </div>
 
-        <div className="nav-item">
-          <NavLink
-            to="/services"
-            className="nav-links"
-            onClick={() => setOpen(false)}
-          >
-            Services
-          </NavLink>
-        </div>
+      </nav>
+        <div className={open ? "nav-menu active" : "nav-menu"}>
+          <div className="nav-item">
+            <NavLink
+              to="/"
+              className="nav-links"
+              onClick={() => setOpen(false)}
+            >
+              Home
+            </NavLink>
+          </div>
 
-        <div className="nav-item">
-          <NavLink
-            to="/products"
-            className="nav-links"
-            onClick={() => setOpen(false)}
-          >
-            Products
-          </NavLink>
+          <div className="nav-item">
+            <NavLink
+              to="/services"
+              className="nav-links"
+              onClick={() => setOpen(false)}
+            >
+              Services
+            </NavLink>
+          </div>
+
+          <div className="nav-item">
+            <NavLink
+              to="/products"
+              className="nav-links"
+              onClick={() => setOpen(false)}
+            >
+              Products
+            </NavLink>
+          </div>
         </div>
-      </div>
-    </nav>
+    </div>
   );
 }
 export default Navbar;
