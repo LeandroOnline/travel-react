6275dc232afefaeedc80dccb4b5c9305ce20e1a2
LeandroOnline <105742869+LeandroOnline@users.noreply.github.com>
2023-02-28 18:06:05 -0300

    feat: JS icon

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,7 +9,7 @@ function About() {
         <img src="https://blog.logrocket.com/wp-content/uploads/2022/11/configuring-service-workers-vitw-2.8.png" alt="" />
         <img src="https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748_1280.png" alt="" />
         <img src="https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582747_960_720.png" alt="" />
-        <img src="https://cdn-icons-png.flaticon.com/512/919/919832.png" alt="" />
+        <img src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png" alt="" />
       </div>
     </div>
   );
